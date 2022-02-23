08ecd31f5fa46fc1597f5d58eb2fec4c6c6b9b92
Yessoufou Yasmine <83721329+YasmineYH@users.noreply.github.com>
2022-02-23 01:55:57 +0100

    Another commit

--- script.js
+++ script.js
@@ -38,19 +38,3 @@ hamburger.addEventListener('click', function() {
         }, 150);
     }
 })
-
-$(document).ready(function(){
-    $("a").on('click', function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-
-            var hash = this.hash;
-            
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function(){
-                window.location.hash = hash;
-            });
-        }
-    });
-});

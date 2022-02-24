449756db44a11ac767ee9743e09e8d4a1b425ef5
Yessoufou Yasmine <83721329+YasmineYH@users.noreply.github.com>
2022-02-24 19:00:35 +0100

    Another commit

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-var hamburger = document.querySelector('.hamburger')
-var hamburgerLine = document.querySelectorAll('.hamburger-line')
-var menu = document.querySelector('nav ul')
 let vh = window.innerHeight * 0.01
 document.documentElement.style.setProperty('--vh', `${vh}px`)
 
@@ -8,6 +5,15 @@ window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01
     document.documentElement.style.setProperty('--vh', `${vh}px`)
 })
+
+
+
+
+
+
+var hamburger = document.querySelector('.hamburger')
+var hamburgerLine = document.querySelectorAll('.hamburger-line')
+var menu = document.querySelector('nav ul')
 
 var clickCounter = 0
 function isEven(value) {
@@ -36,5 +42,87 @@ hamburger.addEventListener('click', function() {
             hamburgerLine[1].style.opacity = '1'
             hamburgerLine[2].style.transform = 'rotate(0) translateY(0) translateX(0)'
         }, 150);
+    }
+})
+
+
+
+
+
+var projectTitle = document.querySelector('.project-header h1')
+var visitBtn = document.querySelector('.project-header .btn')
+var projectImageSmall = document.querySelector('.work-hero .small-hero')
+var projectImageLarge = document.querySelector('.work-hero .regular-hero')
+
+if ((document.location.href).includes('mywork.html')) {
+    var currentPage = ((document.location.href.split('?')[1].split('&'))[0].split('='))[1]
+}
+
+function healthicProject() {
+    var currentPage = 'healthic'
+    window.location.href = 'mywork.html?project=' + encodeURIComponent(currentPage)
+}
+
+function vleProject() {
+    var currentPage = 'vle'
+    window.location.href = 'mywork.html?project=' + encodeURIComponent(currentPage)
+}
+
+function goodpieProject() {
+    var currentPage = 'goodpie'
+    window.location.href = 'mywork.html?project=' + encodeURIComponent(currentPage)
+}
+
+function yassosworldProject() {
+    var currentPage = 'yassosworld'
+    window.location.href = 'mywork.html?project=' + encodeURIComponent(currentPage)
+}
+
+function bicmProject() {
+    var currentPage = 'bicm'
+    window.location.href = 'mywork.html?project=' + encodeURIComponent(currentPage)
+}
+
+window.addEventListener('load', function() {
+    if (currentPage == 'healthic') {
+        projectTitle.innerText = 'Healthic Clinic Application Launch'
+        projectImageLarge.src = 'images/healthic-hero.png'
+        projectImageSmall.src = 'images/healthic-hero-2.png'
+
+        visitBtn.addEventListener('click', function() {
+            window.open("https://www.figma.com/proto/5Ybi1zzgqfJs2M6y8AkwIi/HEALTHIC?page-id=487%3A90&node-id=487%3A91&viewport=380%2C48%2C0.22&scaling=min-zoom&starting-point-node-id=487%3A91", "_blank")
+        })
+    } else if (currentPage == 'vle') {
+        projectTitle.innerText = 'University VLE Web Application'
+        projectImageLarge.src = 'images/vle-hero.png'
+        projectImageSmall.src = 'images/vle-hero-2.png'
+
+        visitBtn.addEventListener('click', function() {
+            window.open("https://univleproject.herokuapp.com", "_blank")
+        })
+    } else if (currentPage == 'goodpie') {
+        projectTitle.innerText = 'GoodPie Food Delivery Landing Page'
+        projectImageLarge.src = 'images/goodpie-hero.png'
+        projectImageSmall.src = 'images/goodpie-hero-2.png'
+
+        visitBtn.addEventListener('click', function() {
+            window.open("https://www.figma.com/proto/gyV0X7DFy6lX2Tn1hOA984/GoodPie?page-id=0%3A1&node-id=1%3A2&viewport=380%2C48%2C0.08&scaling=min-zoom&starting-point-node-id=1%3A2", "_blank")
+        })
+    } else if (currentPage == 'yassosworld') {
+        projectTitle.innerText = 'Yasso’s World Blog Website'
+        projectImageLarge.src = 'images/yassos-hero.png'
+        projectImageSmall.src = 'images/yassos-hero-2.png'
+        
+        visitBtn.addEventListener('click', function() {
+            window.open("https://www.figma.com/proto/eqkVG1JimBr8MVmdfs5MYG/Yasso's-World?page-id=0%3A1&node-id=14%3A29&starting-point-node-id=14%3A29&scaling=scale-down-width", "_blank")
+        })
+    } else if (currentPage == 'bicm') {
+        projectTitle.innerText = 'BICM Consultants Website'
+        projectImageLarge.src = 'images/bicm-hero.png'
+        projectImageSmall.src = 'images/bicm-hero-2.png'
+        
+        visitBtn.addEventListener('click', function() {
+            window.open("https://www.figma.com/proto/Oa5UkSUbGHYYynXYgzihZO/BICM---Final?page-id=608%3A2641&node-id=608%3A2643&viewport=380%2C48%2C0.02&scaling=scale-down-width&starting-point-node-id=608%3A2643&show-proto-sidebar=1", "_blank")
+        })
     }
 })
